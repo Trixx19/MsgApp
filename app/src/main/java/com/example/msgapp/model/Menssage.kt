4cb86e184ef6e65232665870ee2c68d8e4d09194
maria beatriz <mariabeatrizrsf@gmail.com>
2025-07-14 22:17:05 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/example/msgapp/model/Menssage.kt
@@ -0,0 +1,12 @@
+package com.example.msgapp.model
+
+class Menssage {
+    data class Message (
+        val id: String = "",
+        val senderId: String = "",
+        val senderName: String = "",
+        val text: String = "",
+        val timestamp: Long = 0L
+
+    )
+}
